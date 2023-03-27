48bb6fdab1aa684c3d48c2692709dc9a3fc017c2
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-27 15:45:22 +0200

    songs array

---
+++ js/songs.js
@@ -0,0 +1,22 @@
+let allMusic = [
+	{
+		name: 'Ultraviolet',
+		artist: 'Infraction',
+		src: 'music-1',
+	},
+	{
+		name: 'Open Mind',
+		artist: 'Soundroll',
+		src: 'music-2',
+	},
+	{
+		name: 'Infraction',
+		artist: 'Soundroll',
+		src: 'music-3',
+	},
+	{
+		name: 'Fly',
+		artist: 'All Good Folks',
+		src: 'music-4',
+	},
+]
